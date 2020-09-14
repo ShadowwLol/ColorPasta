06b679dee7a604979a330e5bf71b690f3822fa92
Shadoww <57680176+ShadowwLol@users.noreply.github.com>
2020-09-14 22:31:06 +0100

    Add files via upload

---
+++ ColorPasta/ColorPasta/ColorPasta/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Gtk;
+
+namespace Safenotes
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int width = 500;
+            int height = 300;
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Title = "ColorPasta";
+            win.SetSizeRequest(width, height);
+            win.SetDefaultSize(width, height);
+            win.Show();
+            Application.Run();
+        }
+    }
+}
